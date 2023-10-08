2ff75c65f6c5b8731c44c343a2ad2e4092b3f601
GRASiN1 <gauravrajsingh047@gmail.com>
2023-10-08 17:00:56 +0530

    lecture7

---
+++ Assignment.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int password;
+
+    // cout<<"Enter the password: "<<endl;
+    // cin>>password;
+
+    // while(password<999999)
+    // {
+    //     cout<<"The password does not meet the required conditions, Please enter the password again"<<endl;
+    //     cin>>password;
+    // }
+
+    // cout<<"The user has now entered a correct password"<<endl;
+
+    int i;
+    for(i=0; ;i++){
+        cout<<i<<' ';
+        if(i>100)break;
+    }
+
+    return 0;
+
+}
